85067fa0c430d7a5fff438f0933f1e6642d1267e
mikenurthen <125414655+mikenurthen@users.noreply.github.com>
2024-11-21 15:29:12 -0500

    Added hello.py and variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Integer
+launch_count = 10
+
+# Float
+success_rate = 0.95
+
+# String
+rocket_name = "Falcon 9"
+
+# Boolean
+is_active = True
+
+print(launch_count)
+print(success_rate)
+print(rocket_name)
